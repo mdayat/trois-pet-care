50196d49731a45fb2cebad3603dd066a629f7a1e
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-12-29 23:40:11 +0700

    adjust project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
+	server: {
+		port: 3000,
+	},
+	preview: {
+		port: 3000,
+	},
+	build: {
+		sourcemap: true,
+	},
 });
